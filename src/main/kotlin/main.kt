5eb39523e8bde7a7804f0571aa2d1d60fb367597
ramoart <donni.lab.1@gmail.com>
2025-08-01 08:03:03 +0200

    Commit 1

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,13 @@
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import javax.lang.model.util.Elements
+
+
+fun main() {
+    val doc: Document = Jsoup.connect("https://en.wikipedia.org/").get()
+    println(doc.title())
+    val newsHeadLines: org.jsoup.select.Elements = doc.select("#mp-itn b a")
+    println(newsHeadLines)
+
+}
+
