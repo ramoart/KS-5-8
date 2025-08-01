4ab1ceeefda022ce407e9ad823d9aa32321f75da
ramoart <donni.lab.1@gmail.com>
2025-08-01 14:53:31 +0200

    KS-5-8 completed 01.08.25

--- src/main/kotlin/lesson5_task8.kt
+++ src/main/kotlin/lesson5_task8.kt
@@ -3,12 +3,12 @@ import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 
 fun main() {
-    val doc: Document = Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/")
-        .ignoreContentType(true)
-        .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0 Safari/537.36")
-        .get()
+    val doc: Document =
+        Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/")
+            .get()
     val quotes: Elements = doc.select("div.sc-2aegk7-2.bzpNIu")
-    println(quotes)
-
+    for (quote in quotes) {
+        println(quote.text())
+    }
 
 }
